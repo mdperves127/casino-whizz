dd88d269d1d1f663236b1aff0c5da6506aadb06d
perveshossen <shikderm211@gmail.com>
2022-10-22 20:36:30 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$(function () {
+  'use strict';
+
+
+  // WoW Js
+  var wow = new WOW({
+    offset: 2,
+    mobile: false
+  })
+  wow.init();
+
+});
+
+
+
+$('.partner-slider').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
